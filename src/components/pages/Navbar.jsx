225c77fce10062ab22bd63793f04162e239891f3
Sidrajput1 <shekharsidharth7@gmail.com>
2024-04-05 16:40:11 +0530

    add logo and update navbar

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,26 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { RxHamburgerMenu } from "react-icons/rx";
+import logo from '/Users/apple/Desktop/Shimmer Group/Security_Project_with react/Security app/src/assets/images/logo.png'
 function Navbar() {
   return (
     <nav>
-        <div className='flex flex-row w-full  rounded-lg justify-around items-center h-20'>
-            <article className='mr-10 ml-3 relative right-14 font-extrabold italic text-blue-400 text-2xl bg-black shadow-md shadow-slate-700'>SHIMMER
-                  <hr className='font-extrabold text-2xl text-blue-950' />
-                <p className='text-2xl text-red-600'>SECURITY</p>
-                <hr className='font-extrabold text-2xl text-red-950 bg-red-600' />
-            </article>
-            <Link to={"/"} className='text-2xl font-semibold '>
+        <div className='flex flex-row w-full  rounded-lg justify-between  items-center h-20 '>
+            <div className='w-[30%] h-[16vh]  flex flex-row gap-24 items-center '>
+              
+              <img src={logo} alt="logo" className='' />
+              <div>
+              <h1 className='text-4xl text-center text-orange-500 font-bold shadow-sm shadow-orange-500'>SHIMMER </h1>
+                <p className='text-xl relative text-blue-600 text-center mr-2 shadow-sm'> <RxHamburgerMenu className='absolute top-1 right-36 left-4 font-semibold text-[22px]'/>SECURITY  <RxHamburgerMenu className='absolute left-38 right-6  font-semibold bottom-1 text-[22px]' /> </p>
+                <p className='text-[10px] mt-0 font-extralight text-black'>An ISO 9001:2008 Certified Company</p>
+              </div>
+              
+            </div>
+            
+            
+          <div className='flex justify-around w-[60%] h-[10vh] items-center bg-gray-900 text-white font-mono font-extralight hover:bg-orange-300 transition-all ease-in-out duration-300 '>
+            <Link to={"/"} className='text-1xl font-semibold hover:text-amber-950 hover:underline hover:text-2xl transition-all ease-in-out duration-300 '>
                 Home
             </Link>
-            <Link to={"/about"} className='text-2xl'>About</Link>
-            <Link to={"/service"} className='text-2xl'>Service</Link>
-            <Link to={"/contact"} className='text-2xl'>Contact</Link>
-            <Link to={"/galary"} className='text-2xl'>Galary</Link>
-            <Link to={"/testimonial"} className='text-2xl'>Testimonials</Link>
+            <Link to={"/about"} className='text-1xl hover:text-amber-950 hover:underline hover:text-2xl transition-all ease-in-out duration-300'>About</Link>
+            <Link to={"/service"} className='text-1xl hover:text-amber-950 hover:underline hover:text-2xl transition-all ease-in-out duration-300'>Service</Link>
+            <Link to={"/contact"} className='text-1xl hover:text-amber-950 hover:underline hover:text-2xl transition-all ease-in-out duration-300'>Contact</Link>
+            <Link to={"/galary"} className='text-1xl hover:text-amber-950 hover:underline hover:text-2xl transition-all ease-in-out duration-300'>Galary</Link>
+            <Link to={"/testimonial"} className='text-1xl hover:text-amber-950 hover:underline hover:text-2xl transition-all ease-in-out duration-300'>Testimonials</Link>
+          </div>
         </div>
     </nav>
   )
 }
 
 export default Navbar
+
+ {/* <article className='mr-10 ml-3 relative right-14 font-extrabold italic text-blue-400 text-2xl bg-black shadow-md shadow-slate-700'>SHIMMER
+                    <hr className='font-extrabold text-2xl text-blue-950' />
+                  <p className='text-2xl text-red-600'>SECURITY</p>
+                  <hr className='font-extrabold text-2xl text-red-950 bg-red-600' />
+              </article> */}
